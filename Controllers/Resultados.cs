e56281f7268957232efa8cb55689e345e8101184
JOATAN DE JESUS LOPEZ CANUL <20090520@alumno.utmetropolitana.edu.mx>
2021-10-02 12:37:57 -0500

    Agregar archivos de proyecto.

---
+++ Controllers/Resultados.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Polindromos.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Resultados : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult PalindromoResultado(string guardar)
+        {
+            var P = new Palindromo();
+            P.palindromo = guardar;
+            var PFinal = string.Empty;
+            var Pcontar = guardar;
+            int i = Pcontar.Length;
+            var PalindrFinal = "";
+
+            for (int n = i - 1; n >= 0; n--)
+            {
+
+            }
+
+            for (int j = i - 1; j >= 0; j--)
+            {
+                PFinal = PFinal + Pcontar[j];
+            }
+            if (PFinal.ToLower().Replace(" ", string.Empty) == Pcontar.ToLower().Replace(" ", string.Empty))
+            {
+                PalindrFinal = (Pcontar + ": es palindrome");
+            }
+            else
+            {
+                PalindrFinal = (Pcontar + ": no es palindrome");
+            }
+            int Palabras = Pcontar.Length - Pcontar.Replace(" ", string.Empty).Count() + 1;
+            var ResultadoInvert = ("Palabra: " + PalindrFinal.ToLower() + ", Invertido: " + PFinal.ToLower() + "y Numero de palabra: " + Palabras);
+            return Ok(ResultadoInvert.ToLower());
+
+        }
+    }
+}
+
+/*
+ * Universidad Tecnologica Metropolitana
+Mat: Aplicaciones Web Orientadas a Objetos
+Mae: Chuc Uc Joel Ivan
+Actividad: Palindromos
+Alu: Joatan de Jesus Lopez Canul
+4-B
+Parcial 1
+*/
